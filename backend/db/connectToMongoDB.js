343b60a7b1e14f3b8f8b74de5d875ccee0fa1a7c
Shivam Pandey <124254487+shivamworld0608@users.noreply.github.com>
2024-06-20 11:56:10 +0530

    Modified mogodb connect url

--- backend/db/connectToMongoDB.js
+++ backend/db/connectToMongoDB.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectToMongoDB = async () => {
 	try {
-		await mongoose.connect("mongodb://0.0.0.0:27017/ChatAppDB");
+		await mongoose.connect(process.env.MONGODB_URI);
 		console.log("Connected to MongoDB");
 	} catch (error) {
 		console.log("Error connecting to MongoDB", error.message);
