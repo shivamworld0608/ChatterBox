8a05dc2367f4c49ebb76045a6007ee0cae9f59e6
Shivam Pandey <124254487+shivamworld0608@users.noreply.github.com>
2024-06-20 22:55:17 +0530

    Update socket.js

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -8,7 +8,7 @@ const server = http.createServer(app);
 const io = new Server(server, {
 	cors: {
 		origin: ["https://c-box.vercel.app/"],
-		methods: ["GET", "POST"],
+		methods: ["GET", "POST","OPTIONS"],
 	},
 });
 
