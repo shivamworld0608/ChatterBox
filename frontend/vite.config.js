61c196a65afb6d3bb988b2865c0e4cdde3955880
Shivam Pandey <124254487+shivamworld0608@users.noreply.github.com>
2024-06-21 14:07:08 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,19 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import envCompatible from "vite-plugin-env-compatible"
 
-/* // https://vitejs.dev/config/
-export default defineConfig({
-	plugins: [react()],
-	server: {
-		port: 5001,
-		proxy: {
-			"/api": {
-				target: "https://chatterbox-nine.vercel.app/",
-			},
-		},
-	},
-}); */
-
 // https://vitejs.dev/config/
 export default defineConfig({
   envPrefix: "REACT_APP_",
