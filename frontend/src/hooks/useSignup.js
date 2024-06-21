2f99f5f0fe6d229e15adca0ad5e8f82c92cd05e4
Shivam Pandey <124254487+shivamworld0608@users.noreply.github.com>
2024-06-21 19:19:41 +0530

    Update useSignup.js

--- frontend/src/hooks/useSignup.js
+++ frontend/src/hooks/useSignup.js
@@ -27,7 +27,7 @@ const useSignup = () => {
 			localStorage.setItem("chat-user", JSON.stringify(data));
 			setAuthUser(data);
 		} catch (error) {
-			toast.error(error.message);
+			toast.error('Failed to signup: ' + error.message);
 		} finally {
 			setLoading(false);
 		}
