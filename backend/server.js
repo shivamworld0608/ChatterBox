9f72f1bcc90b8f2d49e3b3e80395356a5572a247
Shivam Pandey <124254487+shivamworld0608@users.noreply.github.com>
2024-06-21 13:31:17 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,28 +9,37 @@ import userRoutes from "./routes/user.routes.js";
 
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import { app, server } from "./socket/socket.js";
+import cors from "cors";
+const PORT = process.env.PORT || 5001;
 
-const PORT = process.env.PORT || 5000;
-
-const __dirname = path.resolve();
+/* const __dirname = path.resolve(); */
 
 dotenv.config();
+
+const corsOptions = {
+    origin: '*',
+    credential: true,
+    methods: ["GET", "POST", "OPTIONS"],
+};
+app.options('*', cors(corsOptions));
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/api/auth", authRoutes);
-app.use("/api/messages", messageRoutes);
-app.use("/api/users", userRoutes);
+app.use("/api/auth",cors(corsOptions), authRoutes);
+app.use("/api/messages",cors(corsOptions), messageRoutes);
+app.use("/api/users",cors(corsOptions), userRoutes);
 
 app.get("/", (req, res) => {
     res.send("Hello, this is the root!");
 });
-app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+/* app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
 app.get("*", (req, res) => {
 	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
-});
+}); */
 
 server.listen(PORT, () => {
 	connectToMongoDB();
