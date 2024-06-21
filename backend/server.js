2d521c31f34528e51307b5470074b1614c55ad24
Shivam Pandey <124254487+shivamworld0608@users.noreply.github.com>
2024-06-21 13:11:18 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -23,6 +23,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
+app.get("/",res.send("hello ,how are you")
 app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
 app.get("*", (req, res) => {
