d91dd2916ebe15ed4644d4444120b8b8cffe22f0
Shivam Pandey <124254487+shivamworld0608@users.noreply.github.com>
2024-06-22 09:44:21 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";   
-import mongoose from "mongoose"; 
+import cors from "cors";
 
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
@@ -10,10 +10,8 @@ import userRoutes from "./routes/user.routes.js";
 
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import { app, server } from "./socket/socket.js";
-import cors from "cors";
-const PORT = process.env.PORT || 5001;
 
-/* const __dirname = path.resolve(); */
+const PORT = process.env.PORT || 5001;
 
 dotenv.config();
 
@@ -29,11 +27,13 @@ app.options('*', cors(corsOptions), (req, res) => {
     res.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.sendStatus(200); 
 });
-app.use(cors(corsOptions));
 
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
+
 connectToMongoDB();
+
 app.use("/auth",cors(corsOptions), authRoutes);
 app.use("/messages",cors(corsOptions), messageRoutes);
 app.use("/users",cors(corsOptions), userRoutes);
@@ -42,11 +42,6 @@ app.get("/", (req, res) => {
     res.send("Hello, this is the root!");
 });
 
-/* app.use(express.static(path.join(__dirname, "/frontend/dist")));
-
-app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
-}); */
 server.listen(PORT, () => {
 	connectToMongoDB();
 	console.log(`Server Running on port ${PORT}`);
