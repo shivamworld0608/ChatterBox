ccefb7d673291c64980e85f6cddfff596db1a1b0
Shivam Pandey <124254487+shivamworld0608@users.noreply.github.com>
2024-06-22 15:28:53 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ import userRoutes from "./routes/user.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import { app, server } from "./socket/socket.js";
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5001; 
 
 dotenv.config();
 
