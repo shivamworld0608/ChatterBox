c4c88aeaed8f2ae0690333e6ebfa47006fa43639
Shivam Pandey <124254487+shivamworld0608@users.noreply.github.com>
2024-06-22 09:32:06 +0530

    Update connectToMongoDB.js

--- backend/db/connectToMongoDB.js
+++ backend/db/connectToMongoDB.js
@@ -2,11 +2,11 @@ import mongoose from "mongoose";
 
 const connectToMongoDB = async () => {
 	try {
-		console.log("MongoDB URI:", process.env.MONGOURI);
 		await mongoose.connect(process.env.MONGOURI, {
                 useNewUrlParser: true,
                 useUnifiedTopology: true,
     });
+		console.log("MongoDB URI:", process.env.MONGOURI);
 		console.log("Connected to MongoDB"); 
 	} catch (error) {
 		console.log("Error connecting to MongoDB", error.message);
